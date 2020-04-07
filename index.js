c3ab9fc65f4af4592cdd666e98135ad4f7d2be29
juliaveronica02 <62166022+juliaveronica02@users.noreply.github.com>
2020-04-07 16:48:47 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
+//const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
+fetch('http://openweathermap.org/img/wn/10d@2x.png')
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d')
+    .then(response => {
+        return response.json();
+    })
+
+    .then(data => {
+        console.log(data);
+        const {
+            id,
+            name,
+            weather,
+            wind
+        } = data;
+        const dataCuaca = `
+        <div class="col-sm-12 col-md-12">
+        <img class="img-cuaca" src ="http://openweathermap.org/img/wn/10d@2x.png"/>
+       <h1 class="header-cuaca">${weather[0].main}<h1>
+       <span class="txt-cuaca">
+        id: ${id}
+        <br>
+        city: ${name}
+        <br>
+        wind speed: ${wind.speed}
+        <br>
+        description: ${weather[0].description} </span>
+        </div>
+        `
+        document.getElementById('weather').innerHTML = dataCuaca
+    })
+
