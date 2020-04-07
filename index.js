f35f6334ec6564a2aee470c5fae7ff5d0039c025
Frederick-88 <frederickfd88@gmail.com>
2020-04-07 17:21:59 +0700

    FINAL WEATHER JAKARTA - FREDERICK JULIA

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
+//const URL = 'http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d'
+// fetch('http://openweathermap.org/img/wn/10d@2x.png')
+fetch('http://api.openweathermap.org/data/2.5/weather?q=Jakarta,ID&appid=08c63bb12c5a4132f5d570f08f17872d')
+    .then(response => {
+        return response.json();
+    })
+
+    .then(data => {
+        console.log(data);
+        const {
+            id,
+            name,
+            weather,
+            wind,
+            main
+        } = data;
+        const dataCuaca = `
+        <div class="col-sm-6 col-md-6">
+        <img class="img-cuaca" src ="http://openweathermap.org/img/wn/10d@2x.png"/>
+       
+       </div>
+       <div class="col-sm-6 col-md-6">
+       <h1 class="header-cuaca">${weather[0].main}</h1>
+       <span class="txt-cuaca">
+        id: ${id}
+        <br>
+        City: ${name}
+        <br>
+        Wind Speed: ${wind.speed} KM/H
+        <br>
+        Description: ${weather[0].description}
+        <br>
+        Temperatur: ${main.temp} Kelvin
+        </span>
+        <div class="col-sm-6 col-md-6">
+        `
+        document.getElementById('weather').innerHTML = dataCuaca
+    })
